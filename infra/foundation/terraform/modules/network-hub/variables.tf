96ddb6f2b3a0f5e0f4ab5175d0474ccea3b84dbf
Richard Faulkner <rfaulkner@ferroquesystems.com>
2026-01-07 14:59:56 -0500

    Add placeholder module inputs for foundation skeleton

---
+++ infra/foundation/terraform/modules/network-hub/variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_name" {
+  type        = string
+  description = "Resource group for this module."
+}
+
+variable "location" {
+  type        = string
+  description = "Azure region for this module."
+}
