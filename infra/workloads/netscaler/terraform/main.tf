e09395c1ffc01acff6fecb82ea9df82c461efba6
Richard Faulkner <rfaulkner@ferroquesystems.com>
2026-04-15 11:13:29 -0400

    Automates DNS configuration for AD promotion and enables RDP for NetScaler
    Ensures domain controllers correctly reference the primary DNS server and wait for zone availability before promotion to prevent setup failures. Additionally, updates NetScaler security rules to allow RDP access from authorized subnets and refreshes the infrastructure execution plan.

--- infra/workloads/netscaler/terraform/main.tf
+++ infra/workloads/netscaler/terraform/main.tf
@@ -53,7 +53,7 @@ resource "azurerm_network_security_rule" "terraform-allow-all-from-controlling-s
   access                      = "Allow"
   protocol                    = "Tcp"
   source_port_range           = "*"
-  destination_port_ranges     = ["22", "80", "443"]
+  destination_port_ranges     = ["22", "80", "443", "3389"]
   source_address_prefix       = var.controlling_subnet
   destination_address_prefix  = "*"
   resource_group_name         = azurerm_resource_group.terraform-resource-group.name
@@ -68,7 +68,7 @@ resource "azurerm_network_security_rule" "terraform-allow-all-from-vdi-public-ip
   access                      = "Allow"
   protocol                    = "Tcp"
   source_port_range           = "*"
-  destination_port_ranges     = ["22", "80", "443"]
+  destination_port_ranges     = ["22", "80", "443", "3389"]
   source_address_prefix       = var.vdi_public_ip_cidr
   destination_address_prefix  = "*"
   resource_group_name         = azurerm_resource_group.terraform-resource-group.name
